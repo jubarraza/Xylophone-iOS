e6da375f495601b4cbc6d5e31d89cbb4a65bf3cd
Juli Barraza <jubarraza5@gmail.com>
2024-11-28 00:40:36 -0300

    sonidos completos

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,28 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        playSound(key:sender.currentTitle!)
+    }
     
+    func playSound(key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
+
 
 }
 
